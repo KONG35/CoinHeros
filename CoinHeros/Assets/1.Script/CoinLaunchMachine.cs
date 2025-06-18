3cddab21651819a808964d9b07efa640d2440ef0
KONG35 <annzzang100@naver.com>
2025-06-18 16:54:48 +0900

    no message

---
+++ CoinHeros/Assets/1.Script/CoinLaunchMachine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinLaunchMachine : MonoBehaviour
+{
+    [SerializeField]
+    private Coin copperCoin;
+    [SerializeField]
+    private Coin silverCoin;
+    [SerializeField]
+    private Coin goldCoin;
+    [SerializeField]
+    private Coin diamondCoin;
+
+
+}
