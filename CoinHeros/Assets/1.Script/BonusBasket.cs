558de9b80be6229e94e680cd0ef0b6b35d433e86
KONG35 <annzzang100@naver.com>
2025-06-19 15:13:50 +0900

    no message

--- CoinHeros/Assets/1.Script/BonusBasket.cs
+++ CoinHeros/Assets/1.Script/BonusBasket.cs
@@ -6,21 +6,20 @@ public class BonusBasket : MonoBehaviour
 {
     public float startX = -30f;     // a값
     public float endX = -12f;        // b값
-    public float speed = 0.2f;
 
-    public float period = 10f;  // 왕복에 걸리는 전체 시간 (속도 조절용)
+    public float period = 5f;  // 왕복에 걸리는 전체 시간 (속도 조절용)
 
     private float timer = 0f;
 
-    void Update()
+    void FixedUpdate()
     {
-        //timer += Time.deltaTime;
-        //float normalizedTime = Mathf.PingPong(timer / period, 1f);  // 0~1 왕복
+        timer += Time.deltaTime;
+        float normalizedTime = Mathf.PingPong(timer / period, 1f);  // 0~1 왕복
 
-        //// 양 끝에서 천천히, 중간에서 빠르게 움직이게 만듦
-        //float t = Mathf.SmoothStep(0f, 1f, normalizedTime);
+        // 양 끝에서 천천히, 중간에서 빠르게 움직이게 만듦
+        float t = Mathf.SmoothStep(0f, 1f, normalizedTime);
 
-        //float x = Mathf.Lerp(startX, endX, t);
-        //transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        float x = Mathf.Lerp(startX, endX, t);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 }
--- CoinHeros/Assets/1.Script/Coin.cs
+++ CoinHeros/Assets/1.Script/Coin.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     Rigidbody rigid;
+
     private void Awake()
     {
         rigid = gameObject.GetComponent<Rigidbody>();
+        //rigid.AddForce(Vector3.down * 50f, ForceMode.Force);
+        rigid.velocity = new Vector3(0, -35f, 0);
     }
     // Start is called before the first frame update
     void Start()
@@ -19,13 +22,21 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigid.AddForce(Vector3.down * 10f, ForceMode.Acceleration);
+        //rigid.AddForce(Vector3.down * 10f, ForceMode.Acceleration);
     }
     private void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.tag == "Slider")
         {
             rigid.constraints = RigidbodyConstraints.None;
+
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
+        else if (col.gameObject.tag == "Basket")
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
         }
     }
 }
